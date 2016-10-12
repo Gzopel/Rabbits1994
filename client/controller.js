fed86253b4e7dbf9b57dce294c02419ae83ced00
gzopel <jepunto@gmail.com>
2016-10-12 18:13:43 -0300

    More key bindings

--- client/controller.js
+++ client/controller.js
@@ -63,7 +63,7 @@ var movementUpdate = function(){
 };
 
 var actions = [];
-actions['W']={
+var moveUpKey = {
     down:function () {
         controls.up=true;
         movementUpdate();
@@ -72,16 +72,16 @@ actions['W']={
         controls.up=false;
     }
 };
-actions['S']={
+var moveDownKey = {
     down:function () {
         controls.down=true;
         movementUpdate();
     },
     up:function () {
-    controls.down=false;
+        controls.down=false;
     }
 };
-actions['A']={
+var  moveLeftKey = {
     down:function () {
         controls.left = true;
         movementUpdate();
@@ -90,7 +90,7 @@ actions['A']={
         controls.left=false;
     }
 };
-actions['D']= {
+var moveRightKey = {
     'down': function () {
         controls.right = true;
         movementUpdate();
@@ -107,7 +107,7 @@ actions['J']={
         }
     }
 };
-actions['K']={
+var shootKey = {
     'down':function () {
         if ((ticks-lastAttack) > attackLimit ) {
             lastAttack = ticks;
@@ -115,6 +115,19 @@ actions['K']={
         }
     }
 };
+
+actions['W'] = moveUpKey;
+actions['S'] = moveDownKey;
+actions['A'] = moveLeftKey;
+actions['D'] = moveRightKey;
+actions['UP'] = moveUpKey;
+actions['LEFT'] = moveLeftKey;
+actions['RIGHT'] = moveRightKey;
+actions['DOWN'] = moveDownKey;
+actions['K'] = shootKey;
+actions['ENTER'] = shootKey;
+actions['SPACE'] = shootKey;
+
 module.exports.actions =actions;
 
 /*
