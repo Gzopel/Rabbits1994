0a9b72d09a279a74b7a13bfea970abe8cb66fd06
gzopel <jepunto@gmail.com>
2016-10-16 14:20:30 -0300

    webpack config

---
+++ webpack.prod.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+const base = require('./webpack.base');
+
+base.devtool = 'source-map';
+base.plugins = base.plugins.concat([
+  new webpack.optimize.UglifyJsPlugin({
+    comments: false,
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: "'production'",
+    },
+  }),
+]);
+
+module.exports = base;
